4b2aab013f410b40bba6c93f9f3884b74ab56605
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-31 14:36:24 -0700

    Write conditional logic using #include method to check for inclusion of all of the substrings. Added reverse_sub_word_string to iterate through each substring word in reverse index order as well to account for all substring possibilities. Add logic to assign the substring words to the empty hash. Use #count method to tally number of occurences of each substring in the array. Currently returns the hash with a substring, but the #count method is not returning the correct number of occurences.

--- substrings.rb
+++ substrings.rb
@@ -20,8 +20,25 @@ def substrings(word, array)
   word_array = word.chars
   word_array.each do |char|
     char_index = word_array.index {|element| element == char }
-    p char_index
-    p word_array[0..char_index].join
+    char_index
+     sub_word_str = word_array[0..char_index].join
+     reverse_sub_word_str = word_array[char_index..-1].join
+    if array.include?(sub_word_str) && array.include?(reverse_sub_word_str) == false
+      count = array.count(sub_word_str)
+      hash[sub_word_str] = count
+      return hash
+    elsif array.include?(reverse_sub_word_str) && array.include?(sub_word_str) == false
+      reverse_count = array.count(sub_word_str)
+      hash[reverse_sub_word_str] = reverse_count
+      hash[reverse_sub_word_str] = reverse_count
+      return hash
+    elsif array.include?(sub_word_str) && array.include?(reverse_sub_word_str)
+       count = array.count(sub_word_str)
+      p reverse_count = array.count(reverse_sub_word_str)
+      hash[sub_word_str] = count
+      hash[reverse_sub_word_str] = reverse_count
+      return hash
+    end
   end
 end
   # Declare an empty hash
@@ -29,7 +46,7 @@ end
   # Iterate over the array to
     # 1. Return every possible substring by joining each element one by one per iteration
     # 2. Iterate over the array to check if it includes any of the substrings
-    # 3. If the word is included, start a tally for how many times it is included in the array
+    # 3. If the word is included, start a tally usind #count for how many times it is included in the array
         # store the tally in a variable count
     # 4. Assign hash[:word][:count]
   # Return the hash
